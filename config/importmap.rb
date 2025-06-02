4d9d4d168b6c6dad8c8ba68004be24e8a7b8be79
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-06-02 10:54:28 +0200

    Update importmap cdn

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # Project pin
-pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js", preload: true
 pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js", preload: true
 pin "font-awesome", to: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js", preload: true
