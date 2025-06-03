ed301f7697f3bb2db84a2fb1d334d6048291ddbf
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-06-03 09:34:08 +0200

    Remove home title when story created

--- app/javascript/controllers/story_generator_controller.js
+++ app/javascript/controllers/story_generator_controller.js
@@ -4,8 +4,8 @@ export default class extends Controller {
   static targets = ["story", "title"]
 
   connect() {
-    this.currentPage = 0
-    this.pages = []
+    // this.currentPage = 0
+    // this.pages = []
   }
 
   generate() {
@@ -19,33 +19,36 @@ export default class extends Controller {
     .then(data => {
       document.getElementById("story-title").textContent = data.title;
       document.getElementById("story-content").textContent = data.content;
-      this.pages = this.paginateContent(data.content);
-      this.currentPage = 0;
-      this.displayPage();
+      // this.pages = this.paginateContent(data.content);
+      // this.currentPage = 0;
+      // this.displayPage();
     })
-    .catch(error => console.error("Erreur :", error));
+    .catch(error => console.error("Erreur :", error))
+    .finally(() => {
+      document.getElementById("home-title").classList.add("d-none");
+    });
   }
 
-  paginateContent(content) {
-    // Split story into sentences for pages (adjust as needed)
-    return content.match(/[^.!?]+[.!?]/g) || [content];
-  }
+  // paginateContent(content) {
+  //   // Split story into sentences for pages (adjust as needed)
+  //   return content.match(/[^.!?]+[.!?]/g) || [content];
+  // }
 
-  displayPage() {
-    this.storyTarget.textContent = this.pages[this.currentPage];
-  }
+  // displayPage() {
+  //   this.storyTarget.textContent = this.pages[this.currentPage];
+  // }
 
-  next() {
-    if (this.currentPage < this.pages.length - 1) {
-      this.currentPage++;
-      this.displayPage();
-    }
-  }
+  // next() {
+  //   if (this.currentPage < this.pages.length - 1) {
+  //     this.currentPage++;
+  //     this.displayPage();
+  //   }
+  // }
 
-  prev() {
-    if (this.currentPage > 0) {
-      this.currentPage--;
-      this.displayPage();
-    }
-  }
+  // prev() {
+  //   if (this.currentPage > 0) {
+  //     this.currentPage--;
+  //     this.displayPage();
+  //   }
+  // }
 }
