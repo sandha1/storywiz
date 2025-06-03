be21a1ab025b62b333076d77db43cad392b3df40
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-06-03 09:46:06 +0200

    Remove non functioning code

--- app/javascript/controllers/story_generator_controller.js
+++ app/javascript/controllers/story_generator_controller.js
@@ -3,11 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["story", "title"]
 
-  connect() {
-    // this.currentPage = 0
-    // this.pages = []
-  }
-
   generate() {
     fetch("/generate_story", {
       method: "POST",
@@ -19,36 +14,10 @@ export default class extends Controller {
     .then(data => {
       document.getElementById("story-title").textContent = data.title;
       document.getElementById("story-content").textContent = data.content;
-      // this.pages = this.paginateContent(data.content);
-      // this.currentPage = 0;
-      // this.displayPage();
     })
     .catch(error => console.error("Erreur :", error))
     .finally(() => {
       document.getElementById("home-title").classList.add("d-none");
     });
   }
-
-  // paginateContent(content) {
-  //   // Split story into sentences for pages (adjust as needed)
-  //   return content.match(/[^.!?]+[.!?]/g) || [content];
-  // }
-
-  // displayPage() {
-  //   this.storyTarget.textContent = this.pages[this.currentPage];
-  // }
-
-  // next() {
-  //   if (this.currentPage < this.pages.length - 1) {
-  //     this.currentPage++;
-  //     this.displayPage();
-  //   }
-  // }
-
-  // prev() {
-  //   if (this.currentPage > 0) {
-  //     this.currentPage--;
-  //     this.displayPage();
-  //   }
-  // }
 }
